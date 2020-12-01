4a1620367b9b91a36936f044a3b86f2502fede15
William Baker <wbaker@gmail.com>
2020-12-01 16:19:53 -0500

    App server proxies to dgraph
    Co-authored-by: Ilyas Kussainov <ilkussainov@gmail.com>

--- server/src/server.js
+++ server/src/server.js
@@ -1,19 +1,16 @@
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const morgan = require('morgan');
 
 const app = express();
 const port = 3000;
 
+const proxyToDgraph = createProxyMiddleware({ target: 'http://alpha:8080' });
+
 app.use(morgan('combined'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.get('/graphql', (req, res) => {
-  res.send('Hello from API!');
-});
+app.use('/graphql', proxyToDgraph);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App server listening at http://localhost:${port}`);
 });
