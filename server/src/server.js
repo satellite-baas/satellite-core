e04c67eb80c83efd790443a037f3e9a87572d153
William Baker <wbaker@gmail.com>
2020-12-01 17:54:31 -0500

    Proxied request has correct dgraph header
    Co-authored-by: Lewis Reynolds <lr177245@gmail.com>

--- server/src/server.js
+++ server/src/server.js
@@ -6,15 +6,29 @@ const app = express();
 const port = 3000;
 const API_KEY = process.env.API_KEY;
 
-const proxyToDgraph = createProxyMiddleware({ target: 'http://alpha:8080' });
+const extractTokenFromBearer = (str) => {
+  const parts = str.split(' ');
+  return parts.length == 2 ? parts[1] : '';
+};
 
 const checkAPIKey = (req, res, next) => {
-  console.log(req.header('X-API-Key') === API_KEY);
   if (req.header('X-API-Key') !== API_KEY) {
     throw new Error('invalid credentials');
   }
 
   next();
+};
+
+const proxyToDgraph = (req, res, next) => {
+  const authString = req.headers.authorization;
+  const token = extractTokenFromBearer(authString);
+
+  const proxy = createProxyMiddleware({
+    headers: { 'X-Dgraph-AccessToken': token },
+    target: 'http://alpha:8080',
+  });
+
+  proxy(req, res, next);
 };
 
 app.use(morgan('combined'));
